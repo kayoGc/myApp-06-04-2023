76e52f6ad7ae3364e6a1eb83f47ba49d144807f5
kayoGc <kayivicti@gmail.com>
2023-04-06 20:09:00 -0300

    ligando layouts - 2

--- app/src/main/java/br/com/etecia/myapp/BottomNavActivity.java
+++ app/src/main/java/br/com/etecia/myapp/BottomNavActivity.java
@@ -2,16 +2,26 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BottomNavActivity extends AppCompatActivity {
-    BottomAppBar idBottomappBar;
+    FloatingActionButton idFAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottom_nav_layout);
+        idFAB = findViewById(R.id.idFAB);
+        idFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
